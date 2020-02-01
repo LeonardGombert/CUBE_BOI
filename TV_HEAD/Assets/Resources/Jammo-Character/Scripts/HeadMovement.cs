5be4305f275ca203cad5d31bf41222b053d46aea
Léonard <epoc4oftime@gmail.com>
2020-02-01 22:13:44 +0100

    DEV_Character Movement

--- TV_HEAD/Assets/Resources/Jammo-Character/Scripts/HeadMovement.cs
+++ TV_HEAD/Assets/Resources/Jammo-Character/Scripts/HeadMovement.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,22 +8,64 @@ public class HeadMovement : MonoBehaviour
     public Vector3 startPoint;
     public Vector3 endPoint;
 
+    public float forceValue = 1;
 
     public Rigidbody rb;
+    public SpringJoint springJoint;
+    private bool struckBall;
+
+    float timePassed;
+    float startValue;
+    public float targetValue;
+    float change;
+    public float tweenDuration;
+    public float weakSpringValue;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        startValue = springJoint.spring;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.forward * z + transform.right * x;
+
+
+        if (Input.GetKey(KeyCode.Space))
         {
-            rb.useGravity = true;
-            rb.AddForce(Vector3.forward * 3000 * Time.deltaTime, ForceMode.Impulse);
+            forceValue += 100f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            rb.AddForce(move * forceValue * Time.deltaTime, ForceMode.VelocityChange);
+            struckBall = true;
+            forceValue = 1;
+        }
+
+        if (struckBall) increaseSpringJoint();
+    }
+
+    private void increaseSpringJoint()
+    {
+        springJoint.spring = weakSpringValue;
+
+        timePassed += Time.deltaTime;
+
+        change = targetValue - startValue;
+
+        springJoint.spring = TweenManager.LinearTween(timePassed, startValue, change, tweenDuration);
+
+        if(timePassed >= tweenDuration)
+        {
+            springJoint.spring = startValue;
+            timePassed = 0f;
+            struckBall = false;
         }
     }
 }
