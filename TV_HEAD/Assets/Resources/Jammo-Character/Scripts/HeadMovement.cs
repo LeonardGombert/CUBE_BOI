f346aed0baa54ba829fbdbe729d8940bf2ad2350
Léonard <epoc4oftime@gmail.com>
2020-02-01 19:59:55 +0100

    DEV_Spring Joint Added to Head

---
+++ TV_HEAD/Assets/Resources/Jammo-Character/Scripts/HeadMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadMovement : MonoBehaviour
+{
+    public Vector3 startPoint;
+    public Vector3 endPoint;
+
+
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.useGravity = true;
+            rb.AddForce(Vector3.forward * 3000 * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+}
