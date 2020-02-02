4d98b71b1fef66a547f3c68b7d9d823005bc57b2
Léonard <epoc4oftime@gmail.com>
2020-02-02 15:26:53 +0100

    Push Final (?)

--- TV_HEAD/Assets/Scripts/_LeoScripts/CharacterMovement.cs
+++ TV_HEAD/Assets/Scripts/_LeoScripts/CharacterMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
 
 public class CharacterMovement : MonoBehaviour
 {
@@ -152,6 +153,14 @@ public class CharacterMovement : MonoBehaviour
                 isCentered = true;
                 duration = intitialDuration;
             }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.name == "YES")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
     }
 }
