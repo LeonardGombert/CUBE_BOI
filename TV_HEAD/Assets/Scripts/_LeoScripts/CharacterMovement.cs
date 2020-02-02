916cb33937d4c69ab793b75f53ff2cb2874b33c1
Léonard <epoc4oftime@gmail.com>
2020-02-02 11:44:28 +0100

    Perlin Noise added for Movement

--- TV_HEAD/Assets/Scripts/_LeoScripts/CharacterMovement.cs
+++ TV_HEAD/Assets/Scripts/_LeoScripts/CharacterMovement.cs
@@ -7,21 +7,148 @@ public class CharacterMovement : MonoBehaviour
 {
     CharacterController characterController;
     [SerializeField] float speed = 12f;
+    [SerializeField] float rotateSpeed = 12f;
+    [SerializeField] float rotateAroundSpeed = 45f;
+    [SerializeField] float rotateAroundSpeed2 = 2f;
+
+    float xRotation;
+
+
+    [SerializeField] Quaternion localRotation;
+
+    // Range over which height varies.
+    float heightScale = 10.0f;
+
+    // Distance covered per second along X axis of Perlin plane.
+    float xScale = .25f;
+
+
+    [SerializeField] float height;
+    [SerializeField] float height1;
+    [SerializeField] float height2;
+    [SerializeField] float angleLimit;
+
+
+
+
+
+
+    //INPUT VALUES
+    [SerializeField] float rotationAmount;
+    [SerializeField] float radius;
+
+    [SerializeField] GameObject toMove;
+
+    //TWEEN VALUES
+    public float timePassed;
+    [SerializeField] float startPosition;
+    [SerializeField] float endPosition;
+    float change;
+    float duration;
+    [SerializeField] float shorterDuration;
+    [SerializeField] float intitialDuration;
+
+    float rightPosition = 3.9f;
+    float leftPosition = -3.9f;
+
+    bool resetTime;
+    bool isCentered;
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        isCentered = true;
+        duration = intitialDuration;
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        MyAngle();
+        MyShpere();
+        toMove.transform.position = new Vector3(transform.position.x + rotationAmount, transform.position.y + Mathf.Sqrt(Mathf.Pow(radius, 2) - Mathf.Pow(rotationAmount, 2)), transform.position.z);
+    }
+
+    void MyAngle()
+    {
+        localRotation = transform.rotation;
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
-        characterController.Move(move * speed * Time.deltaTime);
+        height = heightScale * Mathf.PerlinNoise(Time.time * xScale, 0.0f);
+        Vector3 pos = transform.position;
+        pos.x = height;
+        height1 = (float)CustomScaler.Scale(height, 0, 10, -10, 10);
+        //transform.position = new Vector3(height, transform.position.y, transform.position.z);
+
+        if (transform.rotation.eulerAngles.y < 360)
+        {
+            float clampValue = transform.rotation.y + height1;
+            //transform.Rotate(0, height * rotateSpeed/2 * Time.deltaTime, 0, Space.Self);
+            transform.RotateAround(transform.position, transform.up, Mathf.Clamp(clampValue, -10, 10) * rotateAroundSpeed2 * Time.deltaTime);// * rotateAroundSpeed/2 * Time.deltaTime);
+
+            transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed * Time.deltaTime, 0, Space.Self);
+            transform.RotateAround(transform.position, transform.up, Input.GetAxis("Horizontal") * rotateAroundSpeed * Time.deltaTime);
+            Vector3 move3 = transform.forward + transform.right * x + transform.forward * z;
+            characterController.Move(move3 * speed * Time.deltaTime);
+        }
+
+        else
+        {
+            Debug.Log("PD");
+            return;
+        }
+    }
+
+    void MyShpere()
+    {
+        height2 = (float)CustomScaler.Scale(height, 0, 10, -3.9f, 3.9f);
+
+        rotationAmount += height2 * Time.deltaTime;
+
+        timePassed += Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) { timePassed = 0f; resetTime = false; startPosition = rotationAmount; if (rotationAmount != 0f) { isCentered = false; duration = shorterDuration; } }
+        if (Input.GetKey(KeyCode.RightArrow)) { endPosition = rightPosition; }
+        if (Input.GetKeyUp(KeyCode.RightArrow)) { resetTime = true; endPosition = 0; startPosition = 0; }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) { timePassed = 0f; resetTime = false; startPosition = rotationAmount; if (rotationAmount != 0f) { isCentered = false; duration = shorterDuration; } }
+        if (Input.GetKey(KeyCode.LeftArrow)) { endPosition = leftPosition; }
+        if (Input.GetKeyUp(KeyCode.LeftArrow)) { resetTime = true; endPosition = 0; startPosition = 0; }
+
+        change = endPosition - startPosition;
+
+        if (timePassed <= duration && !resetTime)
+        {
+            if (!isCentered) rotationAmount = TweenManager.EaseInQuad(timePassed, startPosition, change, duration);
+            if (isCentered) rotationAmount = TweenManager.EaseInExpo(timePassed, startPosition, change, duration);
+        }
+
+        if (timePassed >= duration || resetTime)
+        {
+            if (rotationAmount > 0f)
+            {
+                rotationAmount -= Time.deltaTime * 1f;
+            }
+            if (rotationAmount < 0f)
+            {
+                rotationAmount += Time.deltaTime * 1f;
+            }
+
+            if (rotationAmount > -0.1f && rotationAmount < 0.1f)
+            {
+                timePassed = 0f;
+                resetTime = false;
+                isCentered = true;
+                duration = intitialDuration;
+            }
+        }
     }
 }
---
+++ TV_HEAD/Assets/Scripts/_LeoScripts/LeanGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeanGenerator : MonoBehaviour
+{
+    // "Bobbing" animation from 1D Perlin noise.
+
+    // Range over which height varies.
+    float heightScale = 10.0f;
+
+    // Distance covered per second along X axis of Perlin plane.
+    float xScale = .25f;
+
+    void Update()
+    {        
+        float height = heightScale * Mathf.PerlinNoise(Time.time * xScale, 0.0f);
+        Vector3 pos = transform.position;
+        pos.x = height;
+        transform.position = new Vector3(height, transform.position.y, transform.position.z);
+
+        Debug.Log(height);
+    }
+}
